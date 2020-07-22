ad8b87a841f80e879a7b527f2d692c5864caaec2
Enes Demirag <ensdmrg@gmail.com>
2020-07-22 18:48:32 +0300

    Update test functions

--- tryhw2.py
+++ tryhw2.py
@@ -1,23 +1,33 @@
 from uwimg import *
 im = load_image("data/dog.jpg")
+
 f = make_box_filter(7)
-blur = convolve_image(im, f, 1)
-save_image(blur, "dog-box7")
+box7 = convolve_image(im, f, 1)
+save_image(box7, "dog-box7")
 
-blur_without_preserve = convolve_image(im, f, 0)
-save_image(blur_without_preserve, "dog-box7-0")
+box_without_preserve = convolve_image(im, f, 0)
+save_image(box_without_preserve, "dog-box7-0")
 
+f = make_box_filter(13)
+bigbox = convolve_image(im, f, 1)
+save_image(bigbox, "dog-box13")
 
-thumb = nn_resize(blur, blur.w//7, blur.h//7)
-save_image(thumb, "dogthumb-nn")
+f = make_highpass_filter()
+high = convolve_image(im, f, 1)
+save_image(high, "dog-highpass")
 
-thumb = bilinear_resize(blur, blur.w//7, blur.h//7)
-save_image(thumb, "dogthumb-bilinear")
+f = make_sharpen_filter()
+sharp = convolve_image(im, f, 1)
+save_image(sharp, "dog-sharp")
+
+f = make_emboss_filter()
+emboss = convolve_image(im, f, 1)
+save_image(emboss, "dog-emboss")
 
 f = make_gaussian_filter(2)
-blur = convolve_image(im, f, 1)
-save_image(blur, "dog-gauss2")
+gauss2 = convolve_image(im, f, 1)
+save_image(gauss2, "dog-gauss2")
 
 f = make_gaussian_filter(3)
-blur = convolve_image(im, f, 1)
-save_image(blur, "dog-gauss3")
+gauss3 = convolve_image(im, f, 1)
+save_image(gauss3, "dog-gauss3")
