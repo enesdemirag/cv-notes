b2b80363fcdf2d31cfff65141064861a6d6c4f6b
Enes Demirag <ensdmrg@gmail.com>
2020-07-22 16:45:40 +0300

    Test when not preserving channels

--- tryhw2.py
+++ tryhw2.py
@@ -2,4 +2,6 @@ from uwimg import *
 im = load_image("data/dog.jpg")
 f = make_box_filter(7)
 blur = convolve_image(im, f, 1)
+blur_without_preserve = convolve_image(im, f, 0)
 save_image(blur, "dog-box7")
+save_image(blur_without_preserve, "dog-box7-0")
