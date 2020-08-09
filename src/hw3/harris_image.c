fd52e55633ebbf5ae9b1c99f8ee9765949656e7a
Enes Demirag <ensdmrg@gmail.com>
2020-08-09 16:37:04 +0300

    Format brackets

--- src/hw3/harris_image.c
+++ src/hw3/harris_image.c
@@ -10,8 +10,7 @@
 // Frees an array of descriptors.
 // descriptor *d: the array.
 // int n: number of elements in array.
-void free_descriptors(descriptor *d, int n)
-{
+void free_descriptors(descriptor *d, int n) {
     int i;
     for(i = 0; i < n; ++i){
         free(d[i].data);
@@ -23,8 +22,7 @@ void free_descriptors(descriptor *d, int n)
 // image im: source image.
 // int i: index in image for the pixel we want to describe.
 // returns: descriptor for that index.
-descriptor describe_index(image im, int i)
-{
+descriptor describe_index(image im, int i) {
     int w = 5;
     descriptor d;
     d.p.x = i%im.w;
@@ -51,8 +49,7 @@ descriptor describe_index(image im, int i)
 // Marks the spot of a point in an image.
 // image im: image to mark.
 // ponit p: spot to mark in the image.
-void mark_spot(image im, point p)
-{
+void mark_spot(image im, point p) {
     int x = p.x;
     int y = p.y;
     int i;
@@ -70,8 +67,7 @@ void mark_spot(image im, point p)
 // image im: image to mark.
 // descriptor *d: corners in the image.
 // int n: number of descriptors to mark.
-void mark_corners(image im, descriptor *d, int n)
-{
+void mark_corners(image im, descriptor *d, int n) {
     int i;
     for(i = 0; i < n; ++i){
         mark_spot(im, d[i].p);
@@ -81,8 +77,7 @@ void mark_corners(image im, descriptor *d, int n)
 // Creates a 1d Gaussian filter.
 // float sigma: standard deviation of Gaussian.
 // returns: single row image of the filter.
-image make_1d_gaussian(float sigma)
-{
+image make_1d_gaussian(float sigma) {
     // TODO: optional, make separable 1d Gaussian.
     return make_image(1,1,1);
 }
@@ -91,8 +86,7 @@ image make_1d_gaussian(float sigma)
 // image im: image to smooth.
 // float sigma: std dev. for Gaussian.
 // returns: smoothed image.
-image smooth_image(image im, float sigma)
-{
+image smooth_image(image im, float sigma) {
     if(1){
         image g = make_gaussian_filter(sigma);
         image s = convolve_image(im, g, 1);
@@ -110,8 +104,7 @@ image smooth_image(image im, float sigma)
 // float sigma: std dev. to use for weighted sum.
 // returns: structure matrix. 1st channel is Ix^2, 2nd channel is Iy^2,
 //          third channel is IxIy.
-image structure_matrix(image im, float sigma)
-{
+image structure_matrix(image im, float sigma) {
     image S = make_image(im.w, im.h, 3);
     
     image Ix = convolve_image(im, make_gx_filter(), 0);
@@ -143,8 +136,7 @@ image structure_matrix(image im, float sigma)
 // Estimate the cornerness of each pixel given a structure matrix S.
 // image S: structure matrix for an image.
 // returns: a response map of cornerness calculations.
-image cornerness_response(image S)
-{
+image cornerness_response(image S) {
     image R = make_image(S.w, S.h, 1);
     // TODO: fill in R, "cornerness" for each pixel using the structure matrix.
     // We'll use formulation det(S) - alpha * trace(S)^2, alpha = .06.
@@ -155,8 +147,7 @@ image cornerness_response(image S)
 // image im: 1-channel image of feature responses.
 // int w: distance to look for larger responses.
 // returns: image with only local-maxima responses within w pixels.
-image nms_image(image im, int w)
-{
+image nms_image(image im, int w) {
     image r = copy_image(im);
     // TODO: perform NMS on the response map.
     // for every pixel in the image:
@@ -173,8 +164,7 @@ image nms_image(image im, int w)
 // int nms: distance to look for local-maxes in response map.
 // int *n: pointer to number of corners detected, should fill in.
 // returns: array of descriptors of the corners in the image.
-descriptor *harris_corner_detector(image im, float sigma, float thresh, int nms, int *n)
-{
+descriptor *harris_corner_detector(image im, float sigma, float thresh, int nms, int *n) {
     // Calculate structure matrix
     image S = structure_matrix(im, sigma);
 
@@ -205,8 +195,7 @@ descriptor *harris_corner_detector(image im, float sigma, float thresh, int nms,
 // float sigma: std. dev for harris.
 // float thresh: threshold for cornerness.
 // int nms: distance to look for local-maxes in response map.
-void detect_and_draw_corners(image im, float sigma, float thresh, int nms)
-{
+void detect_and_draw_corners(image im, float sigma, float thresh, int nms) {
     int n = 0;
     descriptor *d = harris_corner_detector(im, sigma, thresh, nms, &n);
     mark_corners(im, d, n);
